58a786436328d77e5a502aaab87b54ee9a3cbcf0
Dmitrii Toksaitov <toksaitov.d@gmail.com>
2023-09-25 07:54:28 +0200

    Create Vec2 skeleton to run the test file

---
+++ lib/src/main/java/vmath/Vec2.java
@@ -0,0 +1,39 @@
+package vmath;
+
+public class Vec2 {
+    public Vec2() {}
+
+    public Vec2(float x, float y) {}
+
+    public float x() {
+        return 0.0f;
+    }
+
+    public float y() {
+        return 0.0f;
+    }
+
+    public float mag() {
+        return 0.0f;
+    }
+
+    public Vec2 norm() {
+        return null;
+    }
+
+    public Vec2 add(Vec2 other) {
+        return null;
+    }
+
+    public Vec2 sub(Vec2 other) {
+        return null;
+    }
+
+    public Vec2 mul(float other) {
+        return null;
+    }
+
+    public float dot(Vec2 other) {
+        return 0.0f;
+    }
+}
---
+++ lib/src/test/java/vmath/Vec2Test.java
@@ -0,0 +1,136 @@
+package vmath;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Vec2Test {
+
+    @Test
+    void givenNoArgs_whenVec2Created_thenVectorIsZero() {
+        var v = new Vec2();
+        assertEquals(v.x(), 0.0f);
+        assertEquals(v.y(), 0.0f);
+    }
+
+    @Test
+    void givenArgs_whenVec2Created_thenVectorHasGivenValues() {
+        var v = new Vec2(1.0f, 2.0f);
+        assertEquals(v.x(), 1.0f);
+        assertEquals(v.y(), 2.0f);
+    }
+
+    @Test
+    void givenNaNValues_whenVec2Created_thenThrowsException() {
+        assertThrows(ArithmeticException.class, () -> new Vec2(Float.NaN, Float.NaN));
+    }
+
+    @Test
+    void givenZeroVector_whenMagnitudeCalculated_thenResultIsZero() {
+        var v = new Vec2(0.0f, 0.0f);
+        assertEquals(v.mag(), 0.0f);
+    }
+
+    @Test
+    void givenNonZeroVectors_whenMagnitudeCalculated_thenResultsAreCorrect() {
+        var v1 = new Vec2(0.0f, 1.0f);
+        assertEquals(v1.mag(), 1.0f);
+        var v2 = new Vec2(1.0f, 0.0f);
+        assertEquals(v2.mag(), 1.0f);
+        var v3 = new Vec2(0.0f, -1.0f);
+        assertEquals(v3.mag(), 1.0f);
+        var v4 = new Vec2(-1.0f, 0.0f);
+        assertEquals(v4.mag(), 1.0f);
+        var v5 = new Vec2(1.0f, 1.0f);
+        assertEquals(v5.mag(), 1.4142135381698608f);
+    }
+
+    @Test
+    void givenNonZeroVector_whenNormalized_thenResultIsUnitVector() {
+        var v = new Vec2(0.0f, 10.0f).norm();
+        assertEquals(v.x(), 0.0f);
+        assertEquals(v.y(), 1.0f);
+    }
+
+    @Test
+    void givenZeroVector_whenNormalized_thenThrowsException() {
+        assertThrows(ArithmeticException.class, () -> {
+            new Vec2(0.0f, 0.0f).norm();
+        });
+    }
+
+    @Test
+    void givenTwoVectors_whenAdded_thenResultIsCorrect() {
+        var v1 = new Vec2(1.0f, 2.0f);
+        var v2 = new Vec2(2.0f, 3.0f);
+        var sum1 = v1.add(v2);
+        assertEquals(sum1.x(), 3.0f);
+        assertEquals(sum1.y(), 5.0f);
+
+        var v3 = new Vec2(-1.0f, 1.0f);
+        var v4 = new Vec2(1.0f, -1.0f);
+        var sum2 = v3.add(v4);
+        assertEquals(sum2.x(), 0.0f);
+        assertEquals(sum2.y(), 0.0f);
+    }
+
+    @Test
+    void givenTwoVectors_whenSubtracted_thenResultIsCorrect() {
+        var v1 = new Vec2(1.0f, 2.0f);
+        var v2 = new Vec2(2.0f, 3.0f);
+        var diff1 = v1.sub(v2);
+        assertEquals(diff1.x(), -1.0f);
+        assertEquals(diff1.y(), -1.0f);
+
+        var v3 = new Vec2(-1.0f, 1.0f);
+        var v4 = new Vec2(1.0f, -1.0f);
+        var diff2 = v3.sub(v4);
+        assertEquals(diff2.x(), -2.0f);
+        assertEquals(diff2.y(), 2.0f);
+    }
+
+    @Test
+    void givenVectorAndScalar_whenMultiplied_thenResultIsCorrect() {
+        var v1 = new Vec2(1.0f, 2.0f);
+        var prod1 = v1.mul(2.0f);
+        assertEquals(prod1.x(), 2.0f);
+        assertEquals(prod1.y(), 4.0f);
+
+        var v3 = new Vec2(-1.0f, 1.0f);
+        var prod2 = v3.mul(0.0f);
+        assertEquals(prod2.x(), -0.0);
+        assertEquals(prod2.y(), 0.0f);
+    }
+
+    @Test
+    void givenTwoVectors_whenDotProductCalculated_thenResultIsCorrect() {
+        var n1 = new Vec2(1.0f, 2.0f);
+        var l1 = new Vec2(2.0f, 3.0f);
+        var dot1 = n1.dot(l1);
+        assertEquals(dot1, 8.0f);
+
+        var n2 = new Vec2(-1.0f, 1.0f);
+        var l2 = new Vec2(1.0f, -1.0f);
+        var dot2 = n2.dot(l2);
+        assertEquals(dot2, -2.0f);
+    }
+
+    @Test
+    void givenZeroVector_whenConvertedToString_thenResultIsZeroString() {
+        var v1 = new Vec2();
+        assertEquals(v1.toString(), "(0.0, 0.0)");
+    }
+
+    @Test
+    void givenIntParameters_whenConvertedToString_thenResultIsFloatString() {
+        var v2 = new Vec2(1, 2);
+        assertEquals(v2.toString(), "(1.0, 2.0)");
+    }
+
+    @Test
+    void givenRealParameters_whenConvertedToString_thenResultIsCorrect() {
+        var v3 = new Vec2(0.5f, 0.25f);
+        assertEquals(v3.toString(), "(0.5, 0.25)");
+    }
+}
