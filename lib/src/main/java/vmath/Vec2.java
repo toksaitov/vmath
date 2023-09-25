8054892a0617af71d86a88a5d1bc0f62d6a195a0
Dmitrii Toksaitov <toksaitov.d@gmail.com>
2023-09-25 08:07:34 +0200

    Vec2: implement constructors
    Pass tests:
    
    * `givenNoArgs_whenVec2Created_thenVectorIsZero`
    * `givenArgs_whenVec2Created_thenVectorHasGivenValues`

--- lib/src/main/java/vmath/Vec2.java
+++ lib/src/main/java/vmath/Vec2.java
@@ -1,16 +1,24 @@
 package vmath;
 
 public class Vec2 {
-    public Vec2() {}
+    private float x;
+    private float y;
 
-    public Vec2(float x, float y) {}
+    public Vec2() {
+        this(0.0f, 0.0f);
+    }
+
+    public Vec2(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 
     public float x() {
-        return 0.0f;
+        return x;
     }
 
     public float y() {
-        return 0.0f;
+        return y;
     }
 
     public float mag() {
