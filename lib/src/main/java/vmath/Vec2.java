5d5032d5a65b1847d8c66e4332da2ff30a6f4d12
Dmitrii Toksaitov <toksaitov.d@gmail.com>
2023-10-06 05:28:41 +0200

    Vec2: implement `add()` method
    Pass tests:
    
    * `givenTwoVectors_whenAdded_thenResultIsCorrect`

--- lib/src/main/java/vmath/Vec2.java
+++ lib/src/main/java/vmath/Vec2.java
@@ -34,7 +34,7 @@ public class Vec2 {
     }
 
     public Vec2 add(Vec2 other) {
-        return null;
+        return new Vec2(x + other.x, y + other.y);
     }
 
     public Vec2 sub(Vec2 other) {
