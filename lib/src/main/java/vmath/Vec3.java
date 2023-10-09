c53e41bdaa319f60215f4a084974c99cbf8267d1
Dmitrii Toksaitov <toksaitov.d@gmail.com>
2023-10-09 06:02:07 +0200

    Create Vec3 skeleton to run the test file

---
+++ lib/src/main/java/vmath/Vec3.java
@@ -0,0 +1,59 @@
+package vmath;
+
+public class Vec3 {
+    private float x, y, z;
+
+    public Vec3() {
+        this(0.0f, 0.0f, 0.0f);
+    }
+
+    public Vec3(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public float x() {
+        return 0.0f;
+    }
+
+    public float y() {
+        return 0.0f;
+    }
+
+    public float z() {
+        return 0.0f;
+    }
+
+    public float mag() {
+        return 0.0f;
+    }
+
+    public Vec3 norm() {
+        return null;
+    }
+
+    public Vec3 add(Vec3 other) {
+        return null;
+    }
+
+    public Vec3 sub(Vec3 other) {
+        return null;
+    }
+
+    public Vec3 mul(float scalar) {
+        return null;
+    }
+
+    public float dot(Vec3 other) {
+        return 0.0f;
+    }
+
+    public Vec3 cross(Vec3 other) {
+        return null;
+    }
+
+    public String toString() {
+        return "";
+    }
+}
