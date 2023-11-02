db2587e0b85cfc93b11952c33a18242066e732d3
Mukhomediarova Alina <alinocco.dia@gmail.com>
2023-11-02 20:10:31 +0100

    Vec 4: implement all the methods to pass the tests
    Pass tests:
    
    * `givenNoArgs_whenVec4Created_thenVectorIsZero`
    * `givenArgs_whenVec4Created_thenVectorHasGivenValues`
    * `givenNaNValues_whenVec4Created_thenThrowsException`
    * `givenZeroVector_whenMagnitudeCalculated_thenResultIsZero`
    * `givenNonZeroVectors_whenMagnitudeCalculated_thenResultsAreCorrect`
    * `givenNonZeroVector_whenNormalized_thenResultIsUnitVector`
    * `givenZeroVector_whenNormalized_thenThrowsException`
    * `givenTwoVectors_whenAdded_thenResultIsCorrect`
    * `givenTwoVectors_whenSubtracted_thenResultIsCorrect`
    * `givenVectorAndScalar_whenMultiplied_thenResultIsCorrect`
    * `givenTwoVectors_whenDotProductCalculated_thenResultIsCorrect`
    * `givenZeroVector_whenConvertedToString_thenResultIsZeroString`
    * `givenIntParameters_whenConvertedToString_thenResultIsFloatString`
    * `givenRealParameters_whenConvertedToString_thenResultIsCorrect`

--- lib/src/main/java/vmath/Vec4.java
+++ lib/src/main/java/vmath/Vec4.java
@@ -1,13 +1,16 @@
 package vmath;
 
 public class Vec4 {
-    private float x, y, z, w;
+    private final float x, y, z, w;
 
     public Vec4() {
         this(0.0f, 0.0f, 0.0f, 0.0f);
     }
 
     public Vec4(float x, float y, float z, float w) {
+        if (Float.isNaN(x) || Float.isNaN(y) || Float.isNaN(z) || Float.isNaN(w)){
+            throw new ArithmeticException();
+        }
         this.x = x;
         this.y = y;
         this.z = z;
@@ -31,27 +34,28 @@ public class Vec4 {
     }
 
     public float mag() {
-        return 0.0f;
+        return (float) Math.sqrt(x*x + y*y + z*z + w*w);
     }
 
     public Vec4 norm() {
-        return new Vec4();
+        float norm = 1.0f / (float) Math.sqrt(x*x + y*y + z*z + w*w);
+        return new Vec4(x*norm, y*norm, z*norm, w*norm);
     }
 
     public Vec4 add(Vec4 other) {
-        return new Vec4();
+        return new Vec4(x + other.x, y + other.y, z + other.z, w + other.w);
     }
 
     public Vec4 sub(Vec4 other) {
-        return new Vec4();
+        return new Vec4(x - other.x, y - other.y, z - other.z, w - other.w);
     }
 
     public Vec4 mul(float scalar) {
-        return new Vec4();
+        return new Vec4(x * scalar, y * scalar, z * scalar, w * scalar);
     }
 
     public float dot(Vec4 other) {
-        return 0.0f;
+        return x*other.x + y*other.y + z*other.z + w*other.w;
     }
 
     @Override
