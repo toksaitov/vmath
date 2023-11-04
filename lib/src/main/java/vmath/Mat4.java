1044446306112baa76b4b42b5615fb2b82db94d1
Vlad <tsoiivv@gmail.com>
2023-11-05 00:48:48 +0600

    Mat4: implement the Mat4 constructor
    * 'Create a test/s for a constructor `Mat4()` that accepts an array of floats to initialize itself'
    * 'Pass the test/s for the `Mat4()` constructor'

--- lib/src/main/java/vmath/Mat4.java
+++ lib/src/main/java/vmath/Mat4.java
@@ -11,4 +11,22 @@ public class Mat4 {
             0.0f, 0.0f, 0.0f, 1.0f
         };
     }
+
+    public Mat4(float[] other) {
+        if (other.length != 16) {
+            throw new IllegalArgumentException("Input array must have 16 elements");
+        }
+
+        for (float value : other) {
+            if (Float.isNaN(value)) {
+                throw new ArithmeticException();
+            }
+        }
+
+        m = other;
+    }
+
+    public float[] mat() {
+        return m;
+    }
 }
