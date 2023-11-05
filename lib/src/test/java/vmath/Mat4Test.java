61db4b0cd7a8c4b5da4073eada247db196a628b2
Alikhan <alikhan.tursunbekov@gmail.com>
2023-11-05 22:08:26 +0600

    `Mat4`: implement the get(i: int, j: int) method
    * Create a test/s for a method get() that returns a matrix element at a specified i and j indices
    * Pass the test/s for the get() method
    * Finish the default constructor test with a ToDo

--- lib/src/test/java/vmath/Mat4Test.java
+++ lib/src/test/java/vmath/Mat4Test.java
@@ -2,10 +2,26 @@ package vmath;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class Mat4Test {
 
     @Test
     void givenNoArgs_whenMatrix4Created_thenMatrixIsIdentity() {
-        //TODO(): check default constructor for identity matrix using get() method
+        Mat4 m = new Mat4();
+
+        assertEquals(m.get(3,2), 0.0);
+        assertEquals(m.get(0,0), 1.0);
+        assertEquals(m.get(3,3), 1.0);
+    }
+
+    @Test
+    void givenMatrix4_whenGetElementAtInvalidIndices_thenThrowException() {
+        Mat4 m = new Mat4();
+
+        assertThrows(IndexOutOfBoundsException.class, () -> m.get(-1, 2));
+        assertThrows(IndexOutOfBoundsException.class, () -> m.get(1, 5));
+        assertThrows(IndexOutOfBoundsException.class, () -> m.get(4, 3));
     }
 }
