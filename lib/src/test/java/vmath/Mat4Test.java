f30bc41bb6278a0300c5bcfc451bbdea2a48f324
Vlad <tsoiivv@gmail.com>
2023-11-08 21:25:40 +0600

    Mat4: add check for NaN values and test implementation

--- lib/src/test/java/vmath/Mat4Test.java
+++ lib/src/test/java/vmath/Mat4Test.java
@@ -54,4 +54,24 @@ public class Mat4Test {
             }
         }
     }
+
+    @Test
+    void givenNaNValues_whenMat4Created_thenThrowsException() {
+        float[] firstMat = {
+            1.0f, 2.0f, 3.0f, 4.0f,
+            5.0f, 6.0f, Float.NaN, 8.0f,
+            9.0f, 10.0f, 11.0f, 12.0f,
+            13.0f, 14.0f, 15.0f, 16.0f
+        };
+
+        float[] secondMat = {
+            Float.NaN, Float.NaN, Float.NaN, Float.NaN,
+            Float.NaN, Float.NaN, Float.NaN, Float.NaN,
+            Float.NaN, Float.NaN, Float.NaN, Float.NaN,
+            Float.NaN, Float.NaN, Float.NaN, Float.NaN
+        };
+
+        assertThrows(IllegalArgumentException.class, () -> new Mat4(firstMat));
+        assertThrows(IllegalArgumentException.class, () -> new Mat4(secondMat));
+    }
 }
