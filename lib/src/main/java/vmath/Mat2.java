2f2e4a19a1d07798aec901c19274a5ebbdac13f2
Bakyt Yrysov <yrysov_b@auca.kg>
2023-11-08 15:18:30 +0100

    Mat2: implement the Mat2(m: float[]) constructor
    * Create a test/s for a constructor Mat2() that accepts an array of floats to initialize itself
    * Pass the test/s for the Mat2() constructor

--- lib/src/main/java/vmath/Mat2.java
+++ lib/src/main/java/vmath/Mat2.java
@@ -10,6 +10,23 @@ public class Mat2 {
         };
     }
 
+    public Mat2(float[] initialValues) {
+        if (initialValues.length != 4) {
+            throw new IllegalArgumentException("Initial values array must have exactly 4 elements.");
+        }
+        for(int i = 0; i < 4; ++i)
+        {
+            if(Float.isNaN(initialValues[i]))
+            {
+                throw new ArithmeticException();
+            }
+        }
+        m = new float[4];
+        for (int i = 0; i < 4; ++i) {
+            m[i] = initialValues[i];
+        }
+    }
+    
     public float get(int i, int j) {
         if (i < 0 || i >= 2 || j < 0 || j >= 2) {
             throw new IndexOutOfBoundsException("Invalid indices");
