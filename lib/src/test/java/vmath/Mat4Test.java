21e9c636cf2ff9bc1d0d5b68e73c79973d575a2e
Tengiz Turdukulov <turdukulov_t@auca.kg>
2023-11-08 16:44:34 +0100

    Mat4: add check for one NaN value in tests

--- lib/src/test/java/vmath/Mat4Test.java
+++ lib/src/test/java/vmath/Mat4Test.java
@@ -68,5 +68,11 @@ public class Mat4Test {
             Float.NaN, Float.NaN, Float.NaN, Float.NaN,
             Float.NaN, Float.NaN, Float.NaN, Float.NaN
         ));
+        assertThrows(ArithmeticException.class, () -> new Mat4(
+            1.0f, 2.0f, 3.0f, 4.0f,
+            5.0f, 6.0f, Float.NaN, 8.0f,
+            9.0f, 10.0f, 11.0f, 12.0f,
+            13.0f, 14.0f, 15.0f, 16.0f
+        ));
     }
 }
