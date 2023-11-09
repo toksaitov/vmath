b4c95a73bd4243fe2dcb9119f41c78882469d447
Amna Ayub <ayub_a@auca.kg>
2023-11-09 08:18:27 +0100

    Mat3: create tests for a constructor with Array Argument

--- lib/src/test/java/vmath/Mat3Test.java
+++ lib/src/test/java/vmath/Mat3Test.java
@@ -21,6 +21,53 @@ public class Mat3Test {
     }
 
     @Test
+    void givenArrArg_whenMat3Created_thenMatrixHasGivenValues() {
+        var arr = new float[] {
+                1.0f,  2.0f,  3.0f,
+                4.0f,  5.0f,  6.0f,
+                7.0f,  8.0f,  9.0f
+        };
+        var m = new Mat3(arr);
+
+        assertEquals(m.get(0, 0), 1.0f);
+        assertEquals(m.get(0, 1), 2.0f);
+        assertEquals(m.get(0, 2), 3.0f);
+
+        assertEquals(m.get(1, 0), 4.0f);
+        assertEquals(m.get(1, 1), 5.0f);
+        assertEquals(m.get(1, 2), 6.0f);
+
+        assertEquals(m.get(2, 0), 7.0f);
+        assertEquals(m.get(2, 1), 8.0f);
+        assertEquals(m.get(2, 2), 9.0f);
+    }
+
+    @Test
+    void givenWrongArrSize_whenMat3Created_thenThrowsException() {
+        var arr = new float[]{
+                1.0f, 2.0f, 3.0f,
+                4.0f, 5.0f, 6.0f,
+                7.0f, 8.0f, 9.0f, 0.0f
+        };
+        assertThrows(IllegalArgumentException.class, () -> new Mat3(arr));
+    }
+
+    @Test
+    void givenNaNValuesInArr_whenMat3Created_thenThrowsException() {
+        for (int i = 0; i < 9; i++) {
+            var arr = new float[9];
+            arr[i] = Float.NaN;
+            assertThrows(ArithmeticException.class, () -> new Mat3(arr));
+        }
+    }
+
+    @Test
+    void givenNullArr_whenMat3Created_thenThrowsException() {
+        float[] arr = null;
+        assertThrows(IllegalArgumentException.class, () -> new Mat3(arr));
+    }
+
+    @Test
     void givenIndices_whenGetCalled_thenReturnCorrectValue() {
         //TODO
     }
