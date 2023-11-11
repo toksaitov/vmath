9e48da025e4f29ddad7fbc87ce1dcd4bfaf7e565
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2023-11-12 05:45:24 +0600

    Make Mat2Test code consistent with other Matrix classes

--- lib/src/test/java/vmath/Mat2Test.java
+++ lib/src/test/java/vmath/Mat2Test.java
@@ -9,42 +9,64 @@ public class Mat2Test {
 
     @Test
     void givenNoArgs_whenMat2Created_thenMatrixIsIdentity() {
-        Mat2 matrix = new Mat2();
+        var m = new Mat2();
 
-        assertEquals(1.0f, matrix.get(0, 0));
-        assertEquals(0.0f, matrix.get(0, 1));
-        assertEquals(0.0f, matrix.get(1, 0));
-        assertEquals(1.0f, matrix.get(1, 1));
+        assertEquals(m.get(0, 0), 1.0f);
+        assertEquals(m.get(0, 1), 0.0f);
+
+        assertEquals(m.get(1, 0), 0.0f);
+        assertEquals(m.get(1, 1), 1.0f);
+    }
+
+    @Test
+    void givenArgs_whenMat2Created_thenMatrixHasGivenValues() {
+        var m = new Mat2(
+            1.0f, 2.0f,
+            3.0f, 4.0f
+        );
+
+        assertEquals(m.get(0, 0), 1.0f);
+        assertEquals(m.get(0, 1), 2.0f);
+
+        assertEquals(m.get(1, 0), 3.0f);
+        assertEquals(m.get(1, 1), 4.0f);
     }
 
     @Test
     void givenNaNValues_whenMat2Created_thenThrowsException() {
-        assertThrows(ArithmeticException.class, () -> new Mat2 (1.0f, 2.0f, Float.NaN, 4.0f));
-        assertThrows(ArithmeticException.class, () -> new Mat2 (Float.NaN, Float.NaN, Float.NaN, Float.NaN));
+        assertThrows(ArithmeticException.class.class, () -> new Mat2(
+            Float.NaN, Float.NaN,
+            Float.NaN, Float.NaN
+        ));
+
+        assertThrows(ArithmeticException.class.class, () -> new Mat2(
+            1.0f, 2.0f,
+            3.0f, Float.NaN
+        ));
     }
 
     @Test
-    void givenMatrix_whenGetCalledForIndices_thenValuesMatch() {
-        Mat2 matrix = new Mat2(1.0f, 2.0f, 3.0f, 4.0f);
+    void givenIndices_whenGetCalled_thenReturnCorrectValue() {
+        var m = new Mat2(
+            1.0f, 2.0f,
+            3.0f, 4.0f
+        );
 
-        assertEquals(1.0f, matrix.get(0, 0));
-        assertEquals(2.0f, matrix.get(0, 1));
-        assertEquals(3.0f, matrix.get(1, 0));
-        assertEquals(4.0f, matrix.get(1, 1));
+        assertEquals(m.get(0, 0), 1.0f);
+        assertEquals(m.get(0, 1), 2.0f);
+
+        assertEquals(m.get(1, 0), 3.0f);
+        assertEquals(m.get(1, 1), 4.0f);
     }
 
     @Test
-    void givenIndices_whenGetCalled_thenReturnValueMatches() {
-        //TODO
-    }
+    void givenInvalidIndices_whenGetIsCalled_thenThrowsException() {
+        var m = new Mat2();
 
-    @Test
-    void givenInvalidIndices_whenGetCalled_thenThrowIndexOutOfBoundsException() {
-        Mat2 matrix = new Mat2();
+        assertThrows(IndexOutOfBoundsException.class, () -> m.get(-1, 0));
+        assertThrows(IndexOutOfBoundsException.class, () -> m.get(2, 0));
 
-        assertThrows(IndexOutOfBoundsException.class, () -> matrix.get(-1, 0));
-        assertThrows(IndexOutOfBoundsException.class, () -> matrix.get(3, 0));
-        assertThrows(IndexOutOfBoundsException.class, () -> matrix.get(0, -1));
-        assertThrows(IndexOutOfBoundsException.class, () -> matrix.get(0, 3));
+        assertThrows(IndexOutOfBoundsException.class, () -> m.get(0, -1));
+        assertThrows(IndexOutOfBoundsException.class, () -> m.get(0, 2));
     }
 }
