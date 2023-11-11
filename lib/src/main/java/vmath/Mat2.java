fa06a3aa974a696cc364be2d3074076a19a7ac6e
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2023-11-12 04:28:25 +0600

    Make Mat2 code consistent with other Matrix classes

--- lib/src/main/java/vmath/Mat2.java
+++ lib/src/main/java/vmath/Mat2.java
@@ -10,12 +10,12 @@ public class Mat2 {
         };
     }
 
-    public Mat2(float m00, float m01, float m10, float m11) {
-
-        if (Float.isNaN(m00) || Float.isNaN(m01) || Float.isNaN(m10) || Float.isNaN(m11)) {
+    public Mat2(float m00, float m01,
+                float m10, float m11) {
+        if (Float.isNaN(m00) || Float.isNaN(m01) ||
+            Float.isNaN(m10) || Float.isNaN(m11)) {
             throw new ArithmeticException();
         }
-
         m = new float[] {
             m00, m01,
             m10, m11
@@ -24,9 +24,8 @@ public class Mat2 {
 
     public float get(int i, int j) {
         if (i < 0 || i >= 2 || j < 0 || j >= 2) {
-            throw new IndexOutOfBoundsException("Invalid indices");
+            throw new IndexOutOfBoundsException();
         }
-
         return m[i * 2 + j];
     }
 }
