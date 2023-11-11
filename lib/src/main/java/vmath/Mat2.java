42e66048225a3d29d5cd9bde539f5c0f7102e65e
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2023-11-12 04:29:12 +0600

    Make Mat2 array code consistent with other Matrix classes

--- lib/src/main/java/vmath/Mat2.java
+++ lib/src/main/java/vmath/Mat2.java
@@ -12,23 +12,20 @@ public class Mat2 {
 
     public Mat2(float[] m) {
         if (m == null || m.length != 4) {
-            throw new IllegalArgumentException("Array length must be 4 for a 2x2 matrix.");
+            throw new IllegalArgumentException();
         }
-
         for (float value : m) {
             if (Float.isNaN(value)) {
-                throw new IllegalArgumentException("Array contains NaN values.");
+                throw new ArithmeticException();
             }
         }
-
         this.m = m.clone();
     }
 
     public float get(int i, int j) {
         if (i < 0 || i >= 2 || j < 0 || j >= 2) {
-            throw new IndexOutOfBoundsException("Invalid indices");
+            throw new IndexOutOfBoundsException();
         }
-
         return m[i * 2 + j];
     }
 }
