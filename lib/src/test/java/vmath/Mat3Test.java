03586f3c9cc15d47219972f7d521fa28e936ba40
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2023-11-12 05:52:42 +0600

    Make Mat3Test array code consistent with other Matrix classes

--- lib/src/test/java/vmath/Mat3Test.java
+++ lib/src/test/java/vmath/Mat3Test.java
@@ -8,27 +8,29 @@ public class Mat3Test {
 
     @Test
     void givenNoArgs_whenMat3Created_thenMatrixIsIdentity() {
-        var m1 = new Mat3();
+        var m = new Mat3();
 
-        assertEquals(1.0f, m1.get(0, 0));
-        assertEquals(0.0f, m1.get(0, 1));
-        assertEquals(0.0f, m1.get(0, 2));
-        assertEquals(0.0f, m1.get(1, 0));
-        assertEquals(1.0f, m1.get(1, 1));
-        assertEquals(0.0f, m1.get(1, 2));
-        assertEquals(0.0f, m1.get(2, 0));
-        assertEquals(0.0f, m1.get(2, 1));
-        assertEquals(1.0f, m1.get(2, 2));
+        assertEquals(m.get(0, 0), 1.0f);
+        assertEquals(m.get(0, 1), 0.0f);
+        assertEquals(m.get(0, 2), 0.0f);
+
+        assertEquals(m.get(1, 0), 0.0f);
+        assertEquals(m.get(1, 1), 1.0f);
+        assertEquals(m.get(1, 2), 0.0f);
+
+        assertEquals(m.get(2, 0), 0.0f);
+        assertEquals(m.get(2, 1), 0.0f);
+        assertEquals(m.get(2, 2), 1.0f);
     }
 
     @Test
     void givenArrayArg_whenMat3Created_thenMatrixHasGivenValues() {
-        var arr = new float[] {
+        var a = new float[] {
             1.0f, 2.0f, 3.0f,
             4.0f, 5.0f, 6.0f,
             7.0f, 8.0f, 9.0f
         };
-        var m = new Mat3(arr);
+        var m = new Mat3(a);
 
         assertEquals(m.get(0, 0), 1.0f);
         assertEquals(m.get(0, 1), 2.0f);
@@ -42,54 +44,44 @@ public class Mat3Test {
         assertEquals(m.get(2, 1), 8.0f);
         assertEquals(m.get(2, 2), 9.0f);
     }
+    
+    @Test
+    void givenNullArray_whenMat3Created_thenThrowsException() {
+        float[] a = null;
+        assertThrows(IllegalArgumentException.class, () -> new Mat3(a));
+    }
 
     @Test
     void givenArrayOfInvalidLength_whenMat3Created_thenThrowsException() {
-        var arr = new float[] {
+        var a1 = new float[] {
             1.0f, 2.0f, 3.0f,
             4.0f, 5.0f, 6.0f,
-            7.0f, 8.0f, 9.0f, 0.0f
+            7.0f, 8.0f
         };
+        assertThrows(IllegalArgumentException.class, () -> new Mat3(a1));
 
-        assertThrows(IllegalArgumentException.class, () -> new Mat3(arr));
-    }
-
-    @Test
-    void givenNaNValues_whenMat3Created_thenThrowsException() {
-        float[] arrOfNaNValues = {
-            Float.NaN, Float.NaN,
-            Float.NaN, Float.NaN
+        var a2 = new float[] {
+            1.0f, 2.0f, 3.0f,
+            4.0f, 5.0f, 6.0f,
+            7.0f, 8.0f, 9.0f, 10.0f
         };
+        assertThrows(IllegalArgumentException.class, () -> new Mat3(a2));
+    }
 
-        assertThrows(IllegalArgumentException.class, () -> new Mat3(arrOfNaNValues));
-
-        float[] arrWithNaN = {
-            1.0f, 2.0f,
-            Float.NaN, 3.0f
+    @Test
+    void givenArrayWithNaNs_whenMat3Created_thenThrowsException() {
+        float[] a1 = {
+            Float.NaN, Float.NaN, Float.NaN,
+            Float.NaN, Float.NaN, Float.NaN,
+            Float.NaN, Float.NaN, Float.NaN
         };
+        assertThrows(ArithmeticException.class, () -> new Mat3(a1));
 
-        assertThrows(IllegalArgumentException.class, () -> new Mat3(arrWithNaN));
-    }
-
-    @Test
-    void givenNullArray_whenMat3Created_thenThrowsException() {
-        float[] arr = null;
-
-        assertThrows(IllegalArgumentException.class, () -> new Mat3(arr));
-    }
-
-    @Test
-    void givenIndices_whenGetCalled_thenReturnCorrectValue() {
-        //TODO
-    }
-
-    @Test
-    void givenInvalidIndices_whenGetCalled_thenThrowIndexOutOfBoundsException() {
-        var m1 = new Mat3();
-
-        assertThrows(IndexOutOfBoundsException.class, () -> m1.get(-1, 0));
-        assertThrows(IndexOutOfBoundsException.class, () -> m1.get(3, 0));
-        assertThrows(IndexOutOfBoundsException.class, () -> m1.get(0, -1));
-        assertThrows(IndexOutOfBoundsException.class, () -> m1.get(0, 3));
+        float[] a2 = {
+            1.0f, 2.0f, 3.0f,
+            4.0f, 5.0f, 6.0f,
+            7.0f, 8.0f, Float.NaN
+        };
+        assertThrows(ArithmeticException.class, () -> new Mat3(a2));
     }
 }
