1f0826727a4c1044f320c0a84d607d87198c2037
Astra Moldakunova <moldakunova_a@auca.kg>
2023-11-11 19:05:00 +0100

    Mat2Test: correct indentation and test for arithmetic exception

--- lib/src/test/java/vmath/Mat2Test.java
+++ lib/src/test/java/vmath/Mat2Test.java
@@ -18,13 +18,19 @@ public class Mat2Test {
     }
 
     @Test
-    void givenMatrix_whenGetCalledForIndices_thenValuesMatch() {
-    Mat2 matrix = new Mat2(1.0f, 2.0f, 3.0f, 4.0f);
+    void givenNaNValues_whenMat2Created_thenThrowsException() {
+        assertThrows(ArithmeticException.class, () -> new Mat2 (1.0f, 2.0f, Float.NaN, 4.0f));
+        assertThrows(ArithmeticException.class, () -> new Mat2 (Float.NaN, Float.NaN, Float.NaN, Float.NaN));
+    }
 
-    assertEquals(1.0f, matrix.get(0, 0));
-    assertEquals(2.0f, matrix.get(0, 1));
-    assertEquals(3.0f, matrix.get(1, 0));
-    assertEquals(4.0f, matrix.get(1, 1));
+    @Test
+    void givenMatrix_whenGetCalledForIndices_thenValuesMatch() {
+        Mat2 matrix = new Mat2(1.0f, 2.0f, 3.0f, 4.0f);
+
+        assertEquals(1.0f, matrix.get(0, 0));
+        assertEquals(2.0f, matrix.get(0, 1));
+        assertEquals(3.0f, matrix.get(1, 0));
+        assertEquals(4.0f, matrix.get(1, 1));
     }
 
     @Test
