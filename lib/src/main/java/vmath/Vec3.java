c51a9ac3e36d4c402cb473e0b513f08b5e3befff
Basira01 <attai_b@auca.kg>
2023-11-20 15:58:41 +0100

    Improvement to code

--- lib/src/main/java/vmath/Vec3.java
+++ lib/src/main/java/vmath/Vec3.java
@@ -75,7 +75,7 @@ public class Vec3 {
      * @return The normalized (unit) vector.
      */
     public Vec3 norm() {
-        float norm = 1.0f / (float) Math.sqrt(x * x + y * y + z * z);
+        float norm = 1.0f / (float) Math.sqrt(x*x + y*y + z*z);
         return new Vec3(x*norm, y*norm, z*norm);
     }
 
@@ -131,7 +131,8 @@ public class Vec3 {
         return new Vec3(
                 y*other.z() - z*other.y(),
                 z*other.x() - x*other.z(),
-                x*other.y() - y*other.x());
+                x*other.y() - y*other.x()
+	);
     }
 
     /**
