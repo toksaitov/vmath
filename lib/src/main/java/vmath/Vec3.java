335e1d0f8be3ca49b1f18bc794aa829dce0ab211
Basira01 <attai_b@auca.kg>
2023-11-20 15:54:12 +0100

    Delete the spaces around multiplication/division operators in norm(), mul(), dot(), cross() methods.

--- lib/src/main/java/vmath/Vec3.java
+++ lib/src/main/java/vmath/Vec3.java
@@ -76,7 +76,7 @@ public class Vec3 {
      */
     public Vec3 norm() {
         float norm = 1.0f / (float) Math.sqrt(x * x + y * y + z * z);
-        return new Vec3(x * norm, y * norm, z * norm);
+        return new Vec3(x*norm, y*norm, z*norm);
     }
 
     /**
@@ -106,7 +106,7 @@ public class Vec3 {
      * @return The resulting vector after multiplication.
      */
     public Vec3 mul(float scalar) {
-        return new Vec3(x * scalar, y * scalar, z * scalar);
+        return new Vec3(x*scalar, y*scalar, z*scalar);
     }
 
     /**
@@ -117,7 +117,7 @@ public class Vec3 {
      * @return The dot product.
      */
     public float dot(Vec3 other) {
-        return x * other.x + y * other.y + z * other.z;
+        return x*other.x + y*other.y + z*other.z;
     }
 
     /**
@@ -129,9 +129,9 @@ public class Vec3 {
      */
     public Vec3 cross(Vec3 other) {
         return new Vec3(
-                y * other.z() - z * other.y(),
-                z * other.x() - x * other.z(),
-                x * other.y() - y * other.x());
+                y*other.z() - z*other.y(),
+                z*other.x() - x*other.z(),
+                x*other.y() - y*other.x());
     }
 
     /**
