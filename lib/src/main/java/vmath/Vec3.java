7de699f5611d57b3755dde8bdf99114353df3367
Naken Ulukbekov <ulukbekov_n@auca.space>
2023-11-24 19:52:48 +0100

    fixing some mistakes

--- lib/src/main/java/vmath/Vec3.java
+++ lib/src/main/java/vmath/Vec3.java
@@ -56,8 +56,8 @@ public class Vec3 {
     public Vec3 cross(Vec3 other) {
         return new Vec3(
             y*other.z() - z*other.y(),
-	     z*other.x() - x*other.z(),
-	     x*other.y() - y*other.x()
+	    z*other.x() - x*other.z(),
+	    x*other.y() - y*other.x()
         );
     }
 
