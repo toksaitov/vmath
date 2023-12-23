4da832d007f0ffb8275fe4259f8ea5c87d8eb605
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2023-12-23 13:01:12 +0600

    Fix silly syntax errors in Mat3Test.java

--- lib/src/test/java/vmath/Mat3Test.java
+++ lib/src/test/java/vmath/Mat3Test.java
@@ -124,13 +124,13 @@ public class Mat3Test {
     @Test
     void whenToArrayCalled_thenConvertsToArray() {
         var m = new Mat3(
-            1.0f, 2.0f, 3.0f
+            1.0f, 2.0f, 3.0f,
             4.0f, 5.0f, 6.0f,
             7.0f, 8.0f, 9.0f
         );
 
         float[] a1 = {
-            1.0f, 2.0f, 3.0f
+            1.0f, 2.0f, 3.0f,
             4.0f, 5.0f, 6.0f,
             7.0f, 8.0f, 9.0f
         };
