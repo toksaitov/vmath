bcd7f21f9032399e407d09f5b15228cc459b185e
timur <orozmatov_t@auca.kg>
2023-12-29 23:18:26 +0600

    Vec2.java: Impelement JavaDoc comments for methods:
    - norm()
    - add()
    - sub()
    - mul()
    - dot()
    - toString()

--- lib/src/main/java/vmath/Vec2.java
+++ lib/src/main/java/vmath/Vec2.java
@@ -55,27 +55,59 @@ public class Vec2 {
         return (float) Math.sqrt(x*x + y*y);
     }
 
+    /**
+     * Normalizes the vector to have a magnitude of 1.
+     * 
+     * @return A new Vec2 instance representing the normalized vector.
+     */
     public Vec2 norm() {
         float norm = 1.0f / (float) Math.sqrt(x*x + y*y);
         return new Vec2(x*norm, y*norm);
     }
 
+    /**
+     * Adds this vector to another vector.
+     * 
+     * @param other The vector to be added.
+     * @return A new Vec2 instance representing the sum of both vectors.
+     */
     public Vec2 add(Vec2 other) {
         return new Vec2(x + other.x, y + other.y);
     }
 
+    /**
+     * Subtracts another vector from this vector.
+     * 
+     * @param other The vector to be subtracted.
+     * @return A new Vec2 instance representing the difference of both vectors.
+     */
     public Vec2 sub(Vec2 other) {
         return new Vec2(x - other.x, y - other.y);
     }
 
+    /**
+     * Multiplies this vector by a scalar.
+     * 
+     * @param other The scalar to multiply with.
+     * @return A new Vec2 instance representing the scaled vector.
+     */
     public Vec2 mul(float other) {
         return new Vec2(x*other, y*other);
     }
-
+    /**
+     * Calculates the dot product of this vector with another vector.
+     * 
+     * @param other The vector to dot with.
+     * @return The dot product of both vectors.
+     */
     public float dot(Vec2 other) {
         return x*other.x + y*other.y;
     }
-
+    /**
+     * Returns a string representation of the vector in the format (x, y).
+     * 
+     * @return The string representation of the vector.
+     */
     @Override
     public String toString() {
         return "(" + x + ", " + y + ")";
